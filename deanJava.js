10b8594a55eaa7c8e476aaf7a5fe8980d4b15fde
Funnyist Guy <160146093+DeanButCoolest@users.noreply.github.com>
2024-12-16 10:47:41 -0500

    Added flashing (don't add seizure warning it will be funny)

--- deanJava.js
+++ deanJava.js
@@ -1,1 +1,16 @@
+var lightType = 0;
 
+document.getElementById("funny2");
+console.log("test");
+
+function callBorder() {
+    if (lightType === 0) {
+        document.getElementById("funny2").style.border = "4px dotted #f5ff2a";
+        lightType = 1;
+    } else {
+        document.getElementById("funny2").style.border = "4px dotted #c7c8b4";
+        lightType = 0;
+    }
+}
+
+setInterval(callBorder, 200);
